34101676edfdc2a1b98f7463becb72b832e19711
AndreiM <andreim@jukalang.com>
2022-08-18 19:03:56 -0400

    Tizen + Font Update

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -15,17 +15,11 @@ public partial class MainPage : ContentPage
 		SemanticScreenReader.Announce(RunJukaBtn.Text);
         //outputText.Text
 		String data = new Compiler().Go(codeeditor.Text, isFile: false);
-		Webhook.UFWS webManager = new JukaApp.Webhook.UFWS();
-		//webManager.SendWebhook(data,);
+        //webManager.SendWebhook(data,);
 		outputText.Text = data;
         RunJukaBtn.Text = $"Run Juka";
 
     }
 
-	private void codeeditor_TextChanged(object sender, TextChangedEventArgs e)
-	{
-        var oldText = e.OldTextValue;
-        var newText = e.NewTextValue;
-    }
 }
 
